09388c63f382487efc2cc2b955206232a9b2829e
IsraelChacon <51183597+IsraelChacon@users.noreply.github.com>
2023-11-11 12:54:07 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+console.log("PUTOS")
+
+  return(
+    <div>
+      <h1>Invientario super vergas</h1>
+      <p>BABY ON FIRE</p>
+    </div>
+    
+  );
+}
+
+export default App;
